1c2d3d34c04698cc0a5f78bc1e1a543c9fb57714
Thaviru <tsathmira@gmail.com>
2026-03-11 23:17:36 +0530

    🔧 Fix client folder not showing on GitHub - remove nested git repo

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        🏨 Tailwind is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
